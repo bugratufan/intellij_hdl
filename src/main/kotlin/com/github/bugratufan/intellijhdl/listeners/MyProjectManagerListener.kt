d4500abf3c48904211185c754360b196b2088ae8
GitHub Action <action@github.com>
2022-09-16 08:28:14 +0000

    Template cleanup

--- src/main/kotlin/com/github/bugratufan/intellijhdl/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/bugratufan/intellijhdl/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.bugratufan.intellijhdl.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.bugratufan.intellijhdl.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/bugratufan/intellijhdl/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/bugratufan/intellijhdl/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.bugratufan.intellijhdl.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bugratufan.intellijhdl.MyBundle
 
 class MyApplicationService {
 
